77a2d70e3314c45a67f8472ce9315f61417ac574
brianisadog <s254901282002@gmail.com>
2019-02-26 19:25:38 -0800

    Completed

--- rsa_key_cryptosystem.py
+++ rsa_key_cryptosystem.py
@@ -1,14 +1,18 @@
 """
 https://en.wikipedia.org/wiki/RSA_(cryptosystem)
+https://en.wikipedia.org/wiki/Carmichael_function
+https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
 https://gist.github.com/JonCooperWorks/5314103
 """
+
+import random
 
 
 def is_prime(num):
     if num > 1:
         for i in range(2, num):
-            if (num % i) == 0:
-                break
+            if num % i == 0:
+                return False
         return True
     return False
 
@@ -19,15 +23,66 @@ def greatest_common_divisor(a, b):
     return a
 
 
-def key_gen(prime1, prime2):
-    if prime1 == prime2:
-        raise Exception(f"{prime1} == {prime2}")
-    elif not is_prime(prime1) or not is_prime(prime2):
-        raise Exception(f"{prime1} or {prime2} is not prime")
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
 
 
-if __name__ == "__main__":
-    prime1 = input("Input a prime number: ")
-    prime2 = input("Input another different prime number: ")
+def modular_multiplicative_inverse(e, lcm):
+    g, x, y = egcd(e, lcm)
+    if g != 1:
+        raise Exception('Modular inverse does not exist.')
+    else:
+        return x % lcm
 
 
+def key_gen(p, q):
+    if p == q:
+        raise Exception(f'{p} == {q}')
+    elif not is_prime(p) or not is_prime(q):
+        raise Exception(f'{p} or {q} is not prime')
+
+    # Based on Wiki
+    n = p * q
+
+    # Calculating lamda(n) using Carmichael's totient function
+    lcm = (p - 1) * (q - 1)
+    e = random.randrange(1, lcm)
+    gcd = greatest_common_divisor(e, lcm)
+    while gcd != 1:
+        e = random.randrange(1, lcm)
+        gcd = greatest_common_divisor(e, lcm)
+
+    d = modular_multiplicative_inverse(e, lcm)
+
+    return (e, n), (d, n)
+
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+
+    return cipher
+
+
+def decrypt(pk, cipher):
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in cipher]
+
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    prime1 = int(input('Input a prime number: '))
+    prime2 = int(input('Input another different prime number: '))
+    public, private = key_gen(p=prime1, q=prime2)
+    print(f'Public key = {public}')
+    print(f'Private key = {private}')
+
+    text = input('Input a message: ')
+    encrypted = encrypt(private, text)
+    print(f'Encrypted message = {"".join([str(msg) for msg in encrypted])}')
+    print(f'Decrypted message = {decrypt(public, encrypted)}')
